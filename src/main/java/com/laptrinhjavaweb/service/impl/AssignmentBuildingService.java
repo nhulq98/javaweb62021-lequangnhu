991e1e3c54c3dcd51fb0d8a64ccee1c6b91c8841
nhu <nhule2031998@gmail.com>
2021-09-30 20:45:11 +0700

    Update "updateAssignment function"

--- src/main/java/com/laptrinhjavaweb/service/impl/AssignmentBuildingService.java
+++ src/main/java/com/laptrinhjavaweb/service/impl/AssignmentBuildingService.java
@@ -105,57 +105,57 @@ public class AssignmentBuildingService implements IAssignmentBuildingService {
 
 	/**
 	 * ý tưởng:
-     * step1: Tìm những phần tử cùng tồn tại ở cả 2 ds thì xóa ra. Bởi vì
-	 * các ptu này không thay đổi(là không bị bỏ tích ở front-end)
-     * step2: Còn lại ta
-	 * sẽ xóa hết all ptu của danh sách staff load từ DB. Bởi vì những ptu trong ds
-	 * này không có trong ds mà front-end gửi về, có nghĩa là user đã bỏ tích những
-	 * đối tượng này
+     * step1: Tìm những phần tử cùng tồn tại ở cả 2 ds thì bỏ ra.
+     * Bởi vì các ptu này không thay đổi(là không bị bỏ tích ở front-end)
+     * step2: Còn lại ta sẽ xóa hết all ptu của danh sách staff load từ DB.
+     * Bởi vì những ptu trong ds này không có trong ds mà front-end gửi về,
+     * có nghĩa là user đã bỏ tích những đối tượng này
      * step3: Thêm all phần tử của ds staff lấy từ request(front-end).
 	 * Vì những ptu trong ds này không có trong ds load từ DB thì có nghĩa là nó mới
 	 * được tích vào
+     *
 	 *
 	 * @param request
 	 */
-	@Override
-	@Transactional
-	public void updateAssignment(StaffBuildingRequest request) {
-
-		// B1: get List1 staffs is managing this building from database
-		List<AssignmentBuildingEntity> staffsOfBuildingFromDB = assignmentBuildingRepository
-				.findAllByBuildingId(request.getBuildingId());
-
-		// B2: get List2 staff is sent from front-end
-		List<Long> idStaffsFromRequest = request.getStaffIds();
-		// B2.1: load staff data by id
-		List<AssignmentBuildingEntity> staffsFromRequest = loadStaffData(request.getBuildingId(), idStaffsFromRequest);
-
-		// Step1: find element duplicate and remove it
-		for (int i = 0; i < staffsOfBuildingFromDB.size(); i++) {
-			for (int j = 0; j < staffsFromRequest.size(); j++) {
-				if (staffsOfBuildingFromDB.get(i).getUser().getId()
-						.equals(staffsFromRequest.get(j).getUser().getId())) {
-					staffsOfBuildingFromDB.remove(i);
-					staffsFromRequest.remove(j);
-					i--;
-					break;
-				}
-			}
-		}
-
-		deleteAssignmentStaffs(staffsOfBuildingFromDB);
-
-		saveAssignmentStaffs(staffsFromRequest);
-
-		/*
-		 * Mong muốn cải thiện: chỉ dùng 1 vòng lặp để xử lý bài toán
-		 *
-		 * cùng lúc lặp qua 2 list số nguyên và tìm xóa được
-		 * các tu trùng nhau bỏ 2 ds vào 1 mảng kiểm tra nếu 2ptu liên tiếp == nhau thì
-		 * ta bỏ vào 1 mảng trùng. chạy qua mảng trùng và xóa cùng lúc các phần tử trong
-		 * cả 2 danh sách
-		 */
-	}
+//	@Override
+//	@Transactional
+//	public void updateAssignment(StaffBuildingRequest request) {
+//
+//		// B1: get List1 staffs is managing this building from database
+//		List<AssignmentBuildingEntity> staffsOfBuildingFromDB = assignmentBuildingRepository
+//				.findAllByBuildingId(request.getBuildingId());
+//
+//		// B2: get List2 staff is sent from front-end
+//		List<Long> idStaffsFromRequest = request.getStaffIds();
+//		// B2.1: load staff data by id
+//		List<AssignmentBuildingEntity> staffsFromRequest = loadStaffData(request.getBuildingId(), idStaffsFromRequest);
+//
+//		// Step1: find element duplicate and remove it
+//		for (int i = 0; i < staffsOfBuildingFromDB.size(); i++) {
+//			for (int j = 0; j < staffsFromRequest.size(); j++) {
+//				if (staffsOfBuildingFromDB.get(i).getUser().getId()
+//						.equals(staffsFromRequest.get(j).getUser().getId())) {
+//					staffsOfBuildingFromDB.remove(i);
+//					staffsFromRequest.remove(j);
+//					i--;
+//					break;
+//				}
+//			}
+//		}
+//
+//		deleteAssignmentStaffs(staffsOfBuildingFromDB);
+//
+//		saveAssignmentStaffs(staffsFromRequest);
+//
+//		/*
+//		 * Mong muốn cải thiện: chỉ dùng 1 vòng lặp để xử lý bài toán
+//		 *
+//		 * cùng lúc lặp qua 2 list số nguyên và tìm xóa được
+//		 * các tu trùng nhau bỏ 2 ds vào 1 mảng kiểm tra nếu 2ptu liên tiếp == nhau thì
+//		 * ta bỏ vào 1 mảng trùng. chạy qua mảng trùng và xóa cùng lúc các phần tử trong
+//		 * cả 2 danh sách
+//		 */
+//	}
 
 	@Override
 	public void deleteAssignmentStaffs(List<AssignmentBuildingEntity> entities) {
@@ -174,5 +174,42 @@ public class AssignmentBuildingService implements IAssignmentBuildingService {
 		}
 
 	}
+
+    /**
+     * Ý tưởng: chia ra thành 2 danh sách:  Old là bị bỏ chọn, new là được chọn mới
+     * @param request
+     */
+    @Override
+    @Transactional
+    public void updateAssignment(StaffBuildingRequest request) {
+
+		// B1: get staffs Old
+		List<AssignmentBuildingEntity> staffsOld = assignmentBuildingRepository
+				.findAllByBuildingId(request.getBuildingId());
+
+		// B2: get staffs is checked
+		List<Long> idStaffsFromRequest = request.getStaffIds();
+		// B2.1: load staffs data from db
+		List<AssignmentBuildingEntity> staffsNew = loadStaffData(request.getBuildingId(), idStaffsFromRequest);
+
+		// find element duplicate and remove it
+		for (int i = 0; i < staffsOld.size(); i++) {
+			for (int j = 0; j < staffsNew.size(); j++) {
+				if (staffsOld.get(i).getUser().getId()
+						.equals(staffsNew.get(j).getUser().getId())) {
+                    staffsOld.remove(i);
+                    staffsNew.remove(j);
+					i--;
+					break;
+				}
+			}
+		}
+
+		// delete list is unchecked
+		deleteAssignmentStaffs(staffsOld);
+
+        // save list is checked
+		saveAssignmentStaffs(staffsNew);
+    }
 
 }

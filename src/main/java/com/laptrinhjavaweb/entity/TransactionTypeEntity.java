52d8089218b28dff1fd26e1764dc2ab3c33067c0
nhu <nhule2031998@gmail.com>
2021-10-13 17:48:04 +0700

    delete note in transaction type

--- src/main/java/com/laptrinhjavaweb/entity/TransactionTypeEntity.java
+++ src/main/java/com/laptrinhjavaweb/entity/TransactionTypeEntity.java
@@ -19,9 +19,6 @@ public class TransactionTypeEntity extends BaseEntity {
     @Column(name = "code")
     private String code;
 
-    @Column(name = "note")
-    private String note;
-
     @ManyToOne(fetch = FetchType.LAZY)// khai báo kiểu quan hệ
     @JoinColumn(name = "customerid")// Khai báo TÊN khóa ngoại của bảng này sẽ liên kết với đối tượng bên dưới
     private CustomerEntity customer = new CustomerEntity(); // khai báo đối tượng quan hệ
